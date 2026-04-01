71d93c1a3f930fa81f14eed997a9e144b1dd7127
Jack <jacekborucz@gmail.com>
2026-04-01 16:30:39 +0300

    ranking

---
+++ resources/static_db/names/swedish_data.ts
@@ -0,0 +1,43 @@
+// swedishMaleNames.ts
+
+export const SWEDISH_MALE_FIRSTNAMES = [
+  'Noah', 'William', 'Hugo', 'Liam', 'Adam', 'August', 'Nils', 'Leo', 'Oliver', 'Otto',
+  'Sam', 'Alfred', 'Elias', 'Lucas', 'Alexander', 'Emil', 'Oscar', 'Filip', 'Axel', 'Benjamin',
+  'Theo', 'Charlie', 'Max', 'Gabriel', 'Isaac', 'Leon', 'Arvid', 'Viggo', 'Sebastian', 'Milton',
+  'Casper', 'Viktor', 'Henry', 'Elliot', 'Alvin', 'Samuel', 'Adrian', 'Ludvig', 'Erik', 'Anton',
+  'Felix', 'Linus', 'Simon', 'Theodor', 'Malte', 'Gustav', 'Oskar', 'Albin', 'Sixten', 'Ebbe',
+  'Frans', 'Hjalmar', 'Ivar', 'Kasper', 'Loke', 'Melker', 'Rasmus', 'Sigge', 'Tor', 'Wilmer',
+  'Anders', 'Johan', 'Lars', 'Mikael', 'Peter', 'Daniel', 'Jan', 'Per', 'Fredrik', 'Henrik',
+  'Magnus', 'Björn', 'Karl', 'Stefan', 'Thomas', 'Andreas', 'Jonas', 'Mattias', 'Niklas', 'Patrik',
+  'Robin', 'Tobias', 'Christian', 'David', 'Jonathan', 'Marcus', 'Martin', 'Robert', 'Sebastian',
+  'Victor', 'Emmanuel', 'Isak', 'Jakob', 'Joel', 'Kevin', 'Liam', 'Lucas', 'Matteo', 'Noah',
+  'Oliver', 'Philip', 'Rasmus', 'Samuel', 'Tim', 'Vincent', 'Wilhelm', 'Åke', 'Arne', 'Bengt',
+  'Bo', 'Claes', 'Elof', 'Gunnar', 'Hannes', 'Ingvar', 'Jesper', 'Kjell', 'Leif', 'Mats',
+  'Nils', 'Olof', 'Pelle', 'Quintus', 'Ragnar', 'Staffan', 'Tomas', 'Ulf', 'Valdemar', 'Xavier',
+  'Yngve', 'Zacharias', 'Algot', 'Birger', 'Dag', 'Edvin', 'Folke', 'Greger', 'Harald', 'Ivar',
+  'Joakim', 'Kristian', 'Lennart', 'Morgan', 'Nicklas', 'Oskar', 'Pontus', 'Rikard', 'Stig',
+  'Torbjörn', 'Urban', 'Ville', 'Wilfred', 'Xander', 'Yngvar', 'Zlatan'
+];
+
+export const SWEDISH_MALE_LASTNAMES = [
+  'Andersson', 'Johansson', 'Karlsson', 'Nilsson', 'Eriksson', 'Larsson', 'Olsson', 'Persson',
+  'Svensson', 'Gustafsson', 'Pettersson', 'Jonsson', 'Jansson', 'Hansson', 'Bengtsson', 'Carlsson',
+  'Lindberg', 'Magnusson', 'Lindström', 'Berg', 'Axelsson', 'Bergström', 'Nilsson', 'Fredriksson',
+  'Sandberg', 'Sjöberg', 'Lindgren', 'Eriksson', 'Forsberg', 'Bergman', 'Holm', 'Lundberg',
+  'Engström', 'Lindqvist', 'Håkansson', 'Danielsson', 'Eklund', 'Lundgren', 'Björk', 'Bergqvist',
+  'Fransson', 'Nyström', 'Isaksson', 'Arvidsson', 'Söderberg', 'Blom', 'Ekström', 'Martinsson',
+  'Ström', 'Wikström', 'Månsson', 'Åberg', 'Wallin', 'Samuelsson', 'Björklund', 'Norberg',
+  'Mattsson', 'Gunnarsson', 'Nordström', 'Holmberg', 'Eliasson', 'Viklund', 'Sundberg',
+  'Claesson', 'Löfgren', 'Hedlund', 'Jakobsson', 'Andreasson', 'Palm', 'Mårtensson', 'Sandström',
+  'Olofsson', 'Hellström', 'Åkesson', 'Blomberg', 'Lundqvist', 'Ek', 'Söderström', 'Nordin',
+  'Hansson', 'Dahl', 'Falk', 'Grönberg', 'Hedberg', 'Ingvarsson', 'Jönsson', 'Karlsson',
+  'Lind', 'Malm', 'Nord', 'Olsson', 'Pålsson', 'Qvist', 'Rydberg', 'Sjögren', 'Törnqvist',
+  'Ullman', 'Vallin', 'Wahlberg', 'Zetterberg', 'Alm', 'Backman', 'Cederberg', 'Dahlberg',
+  'Edström', 'Fagerström', 'Granberg', 'Hagberg', 'Ivarsson', 'Johansson', 'Karlsson',
+  'Lagerberg', 'Malmberg', 'Norén', 'Oskarsson', 'Persson', 'Qvarnström', 'Rosén', 'Sundström',
+  'Tengberg', 'Ulfsson', 'Vik', 'Westerberg', 'Ylven', 'Zander', 'Åström', 'Öberg', 'Östberg',
+  'Österberg', 'Abrahamsson', 'Beckman', 'Cedervall', 'Dahlgren', 'Ekman', 'Falkenberg',
+  'Granath', 'Hult', 'Isaksson', 'Jansson', 'Kling', 'Ljung', 'Melin', 'Nyman', 'Olausson',
+  'Pettersson', 'Qvist', 'Rasmusson', 'Svensson', 'Thulin', 'Ullberg', 'Vester', 'Wahlgren',
+  'Xenon', 'Ytterberg', 'Zetterlund'
+];
